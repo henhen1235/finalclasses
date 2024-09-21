2e43b6b42e3962901eab1f15dcea852be2ef97dd
Limits <74564867+burgersarefat@users.noreply.github.com>
2024-09-20 21:00:12 -0700

    add comments

--- movies.h
+++ movies.h
@@ -1,23 +1,40 @@
-#include <iostream>
-#include <cstring>
-#include "media.h"
+#include <iostream> // include standard input-output library
+#include <cstring> // include c-string library for string manipulation
+#include "media.h" // include the base media class header file
 
-using namespace std;
+using namespace std; // use the standard namespace
 
-class movies : public media{
- public:
-  movies();
-  virtual char* getdirector();
-  virtual int getduration();
-  virtual float getrating();
-  virtual void setdirector(char* newdirector);
-  virtual void setduration(int newtime);
-  virtual void setrating(float newrating);
-  virtual ~movies(){
-    delete[] director;
-  }
+// declaration of the movies class, derived from the media class
+class movies : public media {
 public:
-  char* director;
-  int time;
-  float rating;
+    // default constructor to initialize the movies object
+    movies();
+    
+    // function to get the director's name
+    virtual char* getdirector();
+    
+    // function to get the duration of the movie
+    virtual int getduration();
+    
+    // function to get the rating of the movie
+    virtual float getrating();
+    
+    // function to set the director's name
+    virtual void setdirector(char* newdirector);
+    
+    // function to set the duration of the movie
+    virtual void setduration(int newtime);
+    
+    // function to set the rating of the movie
+    virtual void setrating(float newrating);
+    
+    // destructor to properly deallocate dynamic memory
+    virtual ~movies() {
+        delete[] director; // delete dynamically allocated memory for director
+    }
+
+public:
+    char* director; // pointer attribute to store the director's name
+    int time; // attribute to store the duration of the movie
+    float rating; // attribute to store the rating of the movie
 };
