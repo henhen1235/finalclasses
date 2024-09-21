bbcf0a410f2f11ad8eaf4812775380cd5021fdb3
Limits <74564867+burgersarefat@users.noreply.github.com>
2024-09-20 21:02:11 -0700

    Update videogames.h

--- videogames.h
+++ videogames.h
@@ -1,20 +1,33 @@
-#include <iostream>
-#include <cstring>
-#include "media.h"
+#include <iostream> // include standard input-output library
+#include <cstring> // include c-string library for string manipulation
+#include "media.h" // include the base media class header file
 
-using namespace std;
+using namespace std; // use the standard namespace
 
-class videogames : public media{
- public:
-  videogames();
-  virtual char* getpublisher();
-  virtual float getrating();
-  virtual void setpublisher(char* newpublisher);
-  virtual void setrating(float newrating);
-  virtual ~videogames(){
-    delete[] publisher;
-  }
- public:
-  char* publisher;
-  float rating;
+// declaration of the videogames class, derived from the media class
+class videogames : public media {
+public:
+    // default constructor to initialize the videogames object
+    videogames();
+    
+    // function to get the publisher of the video game
+    virtual char* getpublisher();
+    
+    // function to get the rating of the video game
+    virtual float getrating();
+    
+    // function to set the publisher of the video game
+    virtual void setpublisher(char* newpublisher);
+    
+    // function to set the rating of the video game
+    virtual void setrating(float newrating);
+    
+    // destructor to properly deallocate dynamic memory
+    virtual ~videogames() {
+        delete[] publisher; // delete dynamically allocated memory for publisher
+    }
+
+public:
+    char* publisher; // pointer attribute to store the publisher's name
+    float rating; // attribute to store the rating of the video game
 };
